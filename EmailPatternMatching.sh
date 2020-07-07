3ecdbd00b31bf1dea3a464e0808be97ef000dc01
Amit Pawar <pawaramitaaa@gmail.com>
2020-07-07 14:26:56 +0530

    Email Pattern matching added

--- EmailPatternMatching.sh
+++ EmailPatternMatching.sh
@@ -1,2 +1,12 @@
-#Email Pattern Matching.
-##!/bin/bash -x
+#Validate Email-id  with a regex.  
+#!/bin/bash -x
+echo "Enter Email-Id:"
+read Email;
+#validate emailid....
+Pat="^[0-9a-zA-Z]+([+_-.][0-9a-zA-Z]+)*@[a-zA-Z0-9]+.[a-zA-Z,]{2,4}([.][0-9a-zA-Z,]{2})";
+if [[ $Email =~ $Pat ]]
+then
+	echo "yes it is valid";
+else
+	echo "no it is not valid";
+fi
